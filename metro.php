fdde68134aeae3ed48e5613731fcb4d206c5c6ac
bfeldman10 <bfeldman10@gmail.com>
2014-08-08 16:54:25 -0400

    Updated metro.php for easier customization

--- metro.php
+++ metro.php
@@ -1,29 +1,49 @@
 <?php
 
-$json = file_get_contents("http://api.wmata.com/StationPrediction.svc/json/GetPrediction/K04?api_key=kfgpmgvfgacx98de9q3xazww");
+// Root URL to get Train Arrival Times
+$apiURL = "http://api.wmata.com/StationPrediction.svc/json/GetPrediction/";
+
+//Station Code; currently set for Ballston. Change this code to lookup different arrival stations.
+$stationCode = "K04";
+
+// API Key for WMATA 
+$apiKey = "kfgpmgvfgacx98de9q3xazww";
+
+//Rail line code you are looking for; currently set for Orange line.
+$lineCode = "OR";
+
+$getURL = $apiURL.$stationCode."?api_key=".$apiKey;
+
+//Get json response
+$json = file_get_contents($getURL);
 
 $decoded = json_decode($json, true);
 
+//Set default output message.
 $message = "Sorry, no trains were found.";
+
+//Iterate json response
 foreach ($decoded["Trains"] as $train)
 {
-    if($train["Line"]=="OR"){
-    	$car = $train["DestinationCode"];
-    	$destCode = $train["DestinationCode"];
+    //Check if arriving train is Orange Line
+    if($train["Line"]==$lineCode){
+    	$destName = $train["DestinationName"];
     	$dest = $train["Destination"];
     	$min = $train["Min"];
-    	if($min=="ARR"){
-    		$message = "The Orange Line train is ARRIVING NOW at Ballston, headed for ".$dest.". Hustle up!!";
-    	}else if($min=="BRD"){
-    		$message = "The Orange Line train is BOARDING NOW at Ballston, headed for ".$dest.". Run!!";
-    	}else{
-    		$message = "The next Orange Line train will arrive at Ballston in ".$min." MINUTES. It's headed for ".$dest.".";
+
+    	if($min=="ARR"){ //Custom message if train is currently arriving
+    		$message = "The Orange Line train is ARRIVING NOW at Ballston, headed for ".$destName.". Hustle up!!";
+    	}else if($min=="BRD"){ //Custom message if train is currently boarding
+    		$message = "The Orange Line train is BOARDING NOW at Ballston, headed for ".$destName.". Run!!";
+    	}else{ 
+    		$message = "The next Orange Line train will arrive at Ballston in ".$min." MINUTES. It's headed for ".$destName.".";
     	}
 
     	break;
     }
 };
 
+//Outputs message with line break for command line
 echo $message."\n";
 
 ?>
